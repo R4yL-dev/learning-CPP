d2755816c0150e9bf72a93ed812455472198950c
Luca Ray <luca.ray@protonmail.ch>
2024-01-22 11:44:03 +0100

    add cpp00/ex00

---
+++ cpp00/ex00/megaphone.cpp
@@ -0,0 +1,48 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lray <lray@student.42lausanne.ch >         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/22 11:07:21 by lray              #+#    #+#             */
+/*   Updated: 2024/01/22 11:42:42 by lray             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <string>
+#include <sstream>
+
+std::string strToUpper(const std::string &str);
+
+int	main(int argc, char **argv)
+{
+	std::string msg;
+
+	if (argc <= 1)
+	{
+		msg = "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+	}
+	else
+	{
+		std::stringstream ss;
+		for (int i = 1; i < argc; ++i)
+		{
+			ss << argv[i] << " ";
+		}
+		msg = ss.str();
+		msg = strToUpper(msg);
+	}
+	std::cout << msg << std::endl;
+	return 0;
+}
+
+std::string strToUpper(const std::string &str)
+{
+	std::string res = str;
+	for (int i = 0; i < res.length(); ++i) {
+		res[i] = std::toupper(str[i]);
+	}
+	return res;
+}
