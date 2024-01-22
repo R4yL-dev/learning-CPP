3f635ae6c1fd1a61992d97f8fcf25420ad7a29c1
Luca Ray <luca.ray@protonmail.ch>
2024-01-22 21:33:06 +0100

    add ex02

---
+++ cpp01/ex02/main.cpp
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lray <lray@student.42lausanne.ch >         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/22 21:23:28 by lray              #+#    #+#             */
+/*   Updated: 2024/01/22 21:32:25 by lray             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <string>
+#include <iostream>
+
+int main()
+{
+	std::string str = "HI THIS IS BRAIN";
+	std::string *stringPTR = &str;
+	std::string &stringREF = str;
+
+	std::cout << "Memory address of str: " << &str << std::endl;
+	std::cout << "Memory address held by stringPTR: " << stringPTR << std::endl;
+	std::cout << "Memory address help by stringREG" << &stringREF << std::endl;
+
+	std::cout << "Value of str: " << str << std::endl;
+	std::cout << "Value pointed by stringPTR: " << *stringPTR << std::endl;
+	std::cout << "Value pointed by stringREF" << stringREF << std::endl;
+
+	return 0;
+}
