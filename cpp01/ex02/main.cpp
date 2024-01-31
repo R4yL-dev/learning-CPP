9637a77581a028b99df3670c1219a69ee35f6926
Luca Ray <luca.ray@protonmail.ch>
2024-01-31 12:38:41 +0100

    clean

--- cpp01/ex02/main.cpp
+++ cpp01/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: lray <lray@student.42lausanne.ch >         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/22 21:23:28 by lray              #+#    #+#             */
-/*   Updated: 2024/01/22 21:32:25 by lray             ###   ########.fr       */
+/*   Updated: 2024/01/31 12:38:19 by lray             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,11 +21,11 @@ int main()
 
 	std::cout << "Memory address of str: " << &str << std::endl;
 	std::cout << "Memory address held by stringPTR: " << stringPTR << std::endl;
-	std::cout << "Memory address help by stringREG" << &stringREF << std::endl;
+	std::cout << "Memory address help by stringREG: " << &stringREF << std::endl;
 
 	std::cout << "Value of str: " << str << std::endl;
 	std::cout << "Value pointed by stringPTR: " << *stringPTR << std::endl;
-	std::cout << "Value pointed by stringREF" << stringREF << std::endl;
+	std::cout << "Value pointed by stringREF: " << stringREF << std::endl;
 
 	return 0;
 }
