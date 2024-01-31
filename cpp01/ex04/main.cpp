c00f6ada122a6d0517139586db81007d860d4031
Luca Ray <luca.ray@protonmail.ch>
2024-01-31 13:14:09 +0100

    clean

--- cpp01/ex04/main.cpp
+++ cpp01/ex04/main.cpp
@@ -6,13 +6,15 @@
 /*   By: lray <lray@student.42lausanne.ch >         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/22 21:42:37 by lray              #+#    #+#             */
-/*   Updated: 2024/01/22 23:19:45 by lray             ###   ########.fr       */
+/*   Updated: 2024/01/31 13:13:58 by lray             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 #include <fstream>
 #include <sstream>
+
+static bool	replace(char **argv);
 
 int main(int argc, char *argv[])
 {
@@ -21,18 +23,22 @@ int main(int argc, char *argv[])
 		std::cerr << "Usage: ./replace filename s1 s2" << std::endl;
 		return 1;
 	}
+	if (!replace(argv))
+		return 1;
+	return 0;
+}
 
+static bool	replace(char **argv)
+{
 	std::ifstream file(argv[1]);
 	if (!file)
 	{
 		std::cerr << "Error: Could not open file " << argv[1] << std::endl;
-		return 1;
+		return false;
 	}
-
 	std::ostringstream ss;
 	ss << file.rdbuf();
 	std::string content = ss.str();
-
 	std::string s1 = argv[2];
 	std::string s2 = argv[3];
 	size_t pos = 0;
@@ -42,15 +48,12 @@ int main(int argc, char *argv[])
 		content.insert(pos, s2);
 		pos += s2.length();
 	}
-
 	std::ofstream outfile(std::string(argv[1]) + ".replace");
 	if (!outfile)
 	{
 		std::cerr << "Error: Could not open output file " << argv[1] << ".replace" << std::endl;
-		return 1;
+		return false;
 	}
-
 	outfile << content;
-
-	return 0;
+	return true;
 }
