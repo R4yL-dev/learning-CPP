4fa60136e2d59e5425918adf9f734cc0f4e4fd3e
Luca Ray <luca.ray@protonmail.ch>
2024-03-06 22:22:37 +0100

    add ex01

---
+++ cpp09/ex01/RPN.hpp
@@ -0,0 +1,23 @@
+#ifndef RPN_HPP
+#define RPN_HPP
+
+#include <iostream>
+#include <sstream>
+#include <stack>
+#include <stdexcept>
+#include <string>
+
+class RPN {
+public:
+  RPN(std::string const &str);
+  RPN(RPN const &src);
+  RPN &operator=(RPN const &rhs);
+  ~RPN();
+
+private:
+  std::stack<int> _stack;
+
+  RPN();
+};
+
+#endif
---
+++ cpp09/ex01/main.cpp
@@ -0,0 +1,25 @@
+#include "RPN.hpp"
+#include <exception>
+#include <iostream>
+
+#define RESET "\e[0m"
+#define RED "\e[31m"
+
+static void showUsage();
+
+int main(int argc, char **argv) {
+  if (argc != 2) {
+    showUsage();
+    return 1;
+  }
+  try {
+    RPN test(argv[1]);
+  } catch (std::exception &err) {
+    std::cout << RED << "[!] " << err.what() << RESET << std::endl;
+  }
+  return 0;
+}
+
+static void showUsage() {
+  std::cout << "Usage: ./RTN  \"<RTN expression>\"" << std::endl;
+}
